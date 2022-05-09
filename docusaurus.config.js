d04ba786f7cc036aa00530ed18c7650acbf19c84
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-05-09 21:19:49 +0200

    Add a docs page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,14 @@ module.exports = {
   title: "MRE: Loader needed",
   url: "http://localhost",
   baseUrl: "/",
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        path: path.resolve(__dirname, "docs"),
+      },
+    ],
+  ],
   themes: [
     "@docusaurus/theme-classic",
     path.resolve(__dirname, "themes/not-found-dev"),
